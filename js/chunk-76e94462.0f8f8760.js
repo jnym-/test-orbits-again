913aaa532da1c32e5e72f1fa784c45ae45c5d2ec
ImpVis <info@impvis.co.uk>
2021-06-25 08:48:36 +0000

    Test with different vis to see if updating works

--- js/chunk-76e94462.0f8f8760.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-76e94462"],{be0f:function(n,w,o){}}]);
-//# sourceMappingURL=chunk-76e94462.0f8f8760.js.map
---
+++ js/chunk-76e94462.30e7b526.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-76e94462"],{be0f:function(n,w,o){}}]);
+//# sourceMappingURL=chunk-76e94462.30e7b526.js.map
